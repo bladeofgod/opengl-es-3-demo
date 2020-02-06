377a6afbe693540a67632d76207edbf14adba5f3
bladeofgod <450103187@qq.com>
2020-02-06 10:04:53 +0800

    1.0

---
+++ app/src/main/java/com/bedrock/opengl_es_30_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bedrock.opengl_es_30_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
